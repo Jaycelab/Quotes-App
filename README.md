311db3ec6da491afc46cdc93ab17ea50586dff04
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-07 01:55:40 -0700

    Finished styling, worked on media query and updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+```markdown
+![Preview Image](https://github.com/Jaycelab/Quotes-App/blob/main/src/images/preview.png)
+```
--- src/components/QuotesApp.jsx
+++ src/components/QuotesApp.jsx
@@ -2,18 +2,20 @@ const QuotesApp = () => {
   return (
     <div className="container">
       <div className="quotes-app">
-        <h1 className="app-heading">Quote</h1>
+        <h1 className="app-heading">A Daily Quote.</h1>
         <i className="bx bxs-heart fav-icon"></i>
 
         <div className="quote">
           <i className="bx bxs-quote-alt-left left-quote"></i>
           <p className="quote-text">
-            Ask not what your country can do for you; ask what you can do for
-            your country
+            Success is peace of mind, which is a direct result of
+            self-satisfaction in knowing you made the effort to become the best
+            of which you are capable.
           </p>
-          <p className="quote-author">John Kennedy</p>
+          <p className="quote-author">John Wooden</p>
           <i className="bx bxs-quote-alt-right right-quote"></i>
         </div>
+
         <div className="circles">
           <div className="circle-1"></div>
           <div className="circle-2"></div>
